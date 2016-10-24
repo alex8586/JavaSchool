8c2dad3db8245565a659b4548af895f5689bf417
algpet <litovec@gmail.com>
2016-10-25 00:56:00 +0300

    restored @Ignore anno over test class

--- src/test/java/lv/javaguru/java3/integrations/rest/RESTResourceTest.java
+++ src/test/java/lv/javaguru/java3/integrations/rest/RESTResourceTest.java
@@ -14,7 +14,7 @@ import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.boot.test.WebIntegrationTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-
+@Ignore
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebIntegrationTest(randomPort = true)
 @SpringApplicationConfiguration(classes = {Application.class})
