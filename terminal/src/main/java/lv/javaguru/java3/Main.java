11f9a6e5a49c4475a945b9c91a45145a74051c6c
algpet <litovec@gmail.com>
2016-11-10 11:19:22 +0200

    termy app - > terminal app

--- terminal/src/main/java/lv/javaguru/java3/Main.java
+++ terminal/src/main/java/lv/javaguru/java3/Main.java
@@ -10,6 +10,11 @@ import lv.javaguru.java3.database.api.RouteResource;
 import lv.javaguru.java3.database.api.TerminalResource;
 import lv.javaguru.java3.dto.RouteDTO;
 
+import java.awt.*;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.StringSelection;
+import java.awt.datatransfer.Transferable;
 import java.util.List;
 
 public class Main {
@@ -25,10 +30,29 @@ public class Main {
                 .target(RouteResource.class, url);
 
         List<RouteDTO> list = routeResource.getAll();
+        String output = "";
         for (RouteDTO routeDTO : list) {
             System.out.println(routeDTO.getId() + " " + routeDTO.getCode() + " " + routeDTO.getName() + " " +
                                routeDTO.getTransportType().getId() + " " + routeDTO.getTransportType().getName() + " " +
                                 routeDTO.getTransportType().getCode());
+            output += routeDTO;
         }
+
+        Toolkit toolkit = Toolkit.getDefaultToolkit();
+        Clipboard clipboard = toolkit.getSystemClipboard();
+        try {
+            String result = (String) clipboard.getData(DataFlavor.stringFlavor);
+            System.out.println("String from Clipboard:" + result);
+
+            Transferable transferable = new StringSelection("booz");
+
+            clipboard.
+
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+
+
     }
 }
